32b41358975dbad43012f7ca306580ff598a6955
Robert B <robert@planetweb.ro>
2023-10-27 14:59:50 +0300

    build

--- build.php
+++ build.php
@@ -1,6 +1,18 @@
 <?php
+require 'src/helpers.php';
+
+$files = get_files_recursive('src');
+$files = array_merge($files, get_files_recursive('vendor'));
+$files[] = 'lumi.php';
+
 $phar = new Phar('lumi.phar');
 $phar->startBuffering();
-$phar->addFile('src/.php');
-$phar->setStub("#!/usr/bin/env php\n" . $phar->createDefaultStub('my_cli.php'));
+
+foreach ( $files as $file ) {
+    $phar->addFile($file);
+}
+
+$phar->setStub("#!/usr/bin/env php\n" . $phar->createDefaultStub('lumi.php'));
 $phar->stopBuffering();
+
+rename('lumi.phar', 'dist/lumi.phar');
